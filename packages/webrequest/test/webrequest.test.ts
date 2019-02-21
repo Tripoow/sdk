1751f7ede57174aaf9c2ce79417ab366d3412457
gully <guglielmo.seminara@gmail.com>
2019-02-21 14:00:45 +0100

    fix(console): useless console log

--- packages/webrequest/test/webrequest.test.ts
+++ packages/webrequest/test/webrequest.test.ts
@@ -19,51 +19,46 @@ describe('WebRequest', () => {
 
     web
       .get('http://api.tripoow.com/', {
-        headers: new Headers([
-          [ 'User-Agent', 'Request-Promise' ]
-        ])
+        headers: new Headers([['User-Agent', 'Request-Promise']])
       })
-      .then(response => {
+      .then((response) => {
         if (typeof response === 'string') {
           response = JSON.parse(response);
         }
-        if (response.status === 200) {
+        /* if (response.status === 200) {
           console.log('body:' + JSON.stringify(response.results));
         } else {
           console.log(response);
-        }
+        } */
         expect(response).toBeTruthy();
       })
-      .catch(error => {
+      .catch((error) => {
         expect(error).toBeTruthy();
       });
 
-      web
+    web
       .get('http://api.tripoow.com/bookings', {
-        headers: new Headers([
-          [ 'User-Agent', 'Request-Promise' ]
-        ])
+        headers: new Headers([['User-Agent', 'Request-Promise']])
       })
       .then((response) => {
         expect(response).toBeTruthy();
       })
-      .catch(error => {
+      .catch((error) => {
         expect(error).toBeInstanceOf(HttpError);
       });
   });
 
   it('tripoow blog', () => {
     const web: WebRequest = new WebRequest();
-    web.get<WArticle[]>('https://tripoow.com/wp-json/wp/v2/posts', {
-      headers: new Headers([
-        [ 'User-Agent', 'Request-Promise']
-      ])
-    })
-    .then((response) => {
-      expect(response).toBeTruthy();
-    })
-    .catch(() => {
-      expect(true).toBe(false);
-    });
+    web
+      .get<WArticle[]>('https://tripoow.com/wp-json/wp/v2/posts', {
+        headers: new Headers([['User-Agent', 'Request-Promise']])
+      })
+      .then((response) => {
+        expect(response).toBeTruthy();
+      })
+      .catch(() => {
+        expect(true).toBe(false);
+      });
   });
 });
